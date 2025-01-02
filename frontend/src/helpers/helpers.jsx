c887f59d1f5fc288b23c5e900fb8ec20548fa8e7
Utku BEKTASOGLU <126083466+utkbkts@users.noreply.github.com>
2025-01-03 00:08:26 +0300

    Update helpers.jsx

--- frontend/src/helpers/helpers.jsx
+++ frontend/src/helpers/helpers.jsx
@@ -45,16 +45,7 @@ export const getDateLocal = (date) => {
 };
 
 export const generateSlug = (input) => {
-  if (typeof input !== "string") {
-    console.warn("Invalid input passed to generateSlug:", input);
-    return "";
-  }
+  let result = input.replace(/-/g, " ");
 
-  return input
-    .split(" ")
-    .filter(Boolean)
-    .map((word, index) =>
-      index === 0 ? word : word.charAt(0).toUpperCase() + word.slice(1)
-    )
-    .join("-");
+  return result;
 };
