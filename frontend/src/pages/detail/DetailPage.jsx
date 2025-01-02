6eb8e1d7219c2ebd7c5212e33d70ba192a5e13e1
Utku BEKTASOGLU <126083466+utkbkts@users.noreply.github.com>
2025-01-03 00:27:45 +0300

    feat(detail):update slug

--- frontend/src/pages/detail/DetailPage.jsx
+++ frontend/src/pages/detail/DetailPage.jsx
@@ -37,7 +37,7 @@ const DetailPage = () => {
             </span>
             <span>on</span>
             <Link
-              to={`/category?=${data?.category}`}
+              to={`/postList?category=${data?.category}`}
               className="text-blue-400 underline"
             >
               {data?.category}
