2c075b6ae858856f31671eecc6d8574b7f15c7be
utku <utkbktss5@gmail.com>
2025-01-26 01:36:37 +0300

    feat(sitemap):sitemap.xml -2

--- backend/routes/generate.sitemap.js
+++ backend/routes/generate.sitemap.js
@@ -1,46 +1,52 @@
+import fs from "fs";
 import Post from "../models/post.model.js";
 import { generateSlug } from "../utils/generateSlug.js";
-import { writeFileSync } from "fs";
+
 export const generateSitemap = async () => {
-  const products = await Post.find();
-  const sitemap = `
-    <?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    <url>
-        <loc>https://www.utkubektasoglu.com/</loc>
-    </url>
-    </url>
-      ${products
-        .map((page) => {
-          return `
-              <url>
-                  <loc>${`https://www.utkubektasoglu.com/details/${generateSlug(
-                    page.title
-                  )}/${page._id}`}</loc>
-              </url>
-            `;
-        })
-        .join("")}
-    <url>
-        <loc>https://www.utkubektasoglu.com/trendingProjects</loc>
-    </url>
-    <url>
-        <loc>https://www.utkubektasoglu.com/trendingBlogs</loc>
-    </url>
-    <url>
-        <loc>https://www.utkubektasoglu.com/about</loc>
-    </url>
-    <url>
-        <loc>https://www.utkubektasoglu.com/auth/signup</loc>
-    </url>
-    <url>
-        <loc>https://www.utkubektasoglu.com/auth/signin</loc>
-    </url>
-    <url>
-        <loc>https://www.utkubektasoglu.com/contact</loc>
-    </url>
-</urlset>
-`;
-  writeFileSync("./frontend/public/sitemap.xml", sitemap);
-  return true;
+  try {
+    const products = await Post.find();
+
+    const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>https://www.utkubektasoglu.com/</loc>
+  </url>
+  ${products
+    .map((page) => {
+      return `
+  <url>
+    <loc>https://www.utkubektasoglu.com/details/${generateSlug(page.title)}/${
+        page._id
+      }</loc>
+  </url>`;
+    })
+    .join("")}
+  <url>
+    <loc>https://www.utkubektasoglu.com/trendingProjects</loc>
+  </url>
+  <url>
+    <loc>https://www.utkubektasoglu.com/trendingBlogs</loc>
+  </url>
+  <url>
+    <loc>https://www.utkubektasoglu.com/about</loc>
+  </url>
+  <url>
+    <loc>https://www.utkubektasoglu.com/auth/signup</loc>
+  </url>
+  <url>
+    <loc>https://www.utkubektasoglu.com/auth/signin</loc>
+  </url>
+  <url>
+    <loc>https://www.utkubektasoglu.com/contact</loc>
+  </url>
+</urlset>`.trim();
+
+    fs.writeFileSync("./frontend/public/sitemap.xml", sitemap);
+    console.log("Generated Sitemap successfully");
+
+    return true;
+  } catch (error) {
+    console.error("Error generating sitemap:", error);
+    return false;
+  }
 };
